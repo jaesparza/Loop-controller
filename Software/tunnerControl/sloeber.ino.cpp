ec894a5e3336ac2728667ad159b0a51859e4777c
José Antonio Esparza Isasa <jaesparza@gmail.com>
2018-06-16 18:06:15 +0200

    Added sloeber project

---
+++ Software/tunnerControl/sloeber.ino.cpp
@@ -0,0 +1,23 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2018-06-16 17:33:44
+
+#include "Arduino.h"
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+#include "HardwareConfig.h"
+
+void setup() ;
+bool configMode() ;
+void sendPulse() ;
+void readInputs();
+void loop() ;
+void configurationOperation() ;
+void normalOperation() ;
+
+
+#include "LoopControllerRevB.ino"
+
+#endif
